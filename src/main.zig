c7d27d699b119adede762167c2c40007a9b5efd6
setPedro <pedross4789@gmail.com>
2025-01-28 23:12:27 +0100

    format path & expected_output

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+const DIR_ENTRY = "├──";
+const DIR_GAP = "|  ";
+const FILE_ENTRY = "└──";
+
 /// A simple program to display the folder and file structure of a directory in a readable way
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -30,7 +34,14 @@ fn iterateDir(allocator: std.mem.Allocator, path: []const u8) !void {
             const _path = try std.fs.path.join(allocator, &.{ path, entry.name });
 
             defer allocator.free(_path);
-            std.debug.print("PATH: {s}\n", .{_path});
+
+            const result = try formatPath(_path);
+            const indent_level = result[0];
+            const formatted_path = result[1];
+            _ = indent_level;
+            _ = formatted_path;
+
+            std.debug.print("{s}\n", .{_path});
 
             try iterateDir(allocator, _path);
         }
@@ -47,4 +58,17 @@ fn lessThan(_: void, lhs: std.fs.Dir.Entry, rhs: std.fs.Dir.Entry) bool {
     }
 
     return std.mem.lessThan(u8, lhs.name, rhs.name);
+}
+
+fn formatPath(path: []u8) !struct { u8, []u8 } {
+    var level: u8 = 0;
+    var formatted_path: []u8 = "";
+    for (path, 0..) |char, i| {
+        if (char == '/') {
+            formatted_path = path[i + 1 ..];
+            level += 1;
+        }
+    }
+
+    return .{ level, formatted_path };
 }
